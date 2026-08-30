abf0d5baee561de9b8c8fd3765b66fbfb37a6de1
joaovictormnzs <jvictormenezesl@gmail.com>
2026-08-30 13:26:19 -0300

    express-mongo: creating the firsts routes

--- 3-express-mongo/server.js
+++ 3-express-mongo/server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000; // -> porta de comunicacao
 
+const rotas = {
+    "/": "Curso de Express API",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
+
 const server = http.createServer((req, res ) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Curso de Node.js");
+    res.end(rotas[req.url]); // --> url: propriedade do objeto req
 });
 
 server.listen(PORT, () => {
