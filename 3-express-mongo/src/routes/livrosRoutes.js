dd75642133992184d6adfdd26fda816e34360fb0
joaovictormnzs <jvictormenezesl@gmail.com>
2026-09-04 13:36:24 -0300

    express-mongo: refactoring and adding a controller

---
+++ 3-express-mongo/src/routes/livrosRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import LivroController from "../controller/livroController.js";
+
+const routes = express.Router();
+
+routes.get("/livros", LivroController.listarLivros);
+
+export default routes;
