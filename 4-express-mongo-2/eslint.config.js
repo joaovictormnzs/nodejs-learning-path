2c799d9a88467f75f2494aa505ff59b53aebd4e1
joaovictormnzs <jvictormenezesl@gmail.com>
2026-09-10 08:06:20 -0300

    express-mongo-2: adding eslint configs

---
+++ 4-express-mongo-2/eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2021,
+            },
+        },
+        rules: {
+            indent: ["error", 4],
+            semi: ["error", "always"],
+        },
+    },
+]);
