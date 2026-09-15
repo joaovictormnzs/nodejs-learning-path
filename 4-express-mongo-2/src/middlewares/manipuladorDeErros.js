dce0ccc4f8b87ee02cbce614f06ebb76dcae81f9
joaovictormnzs <jvictormenezesl@gmail.com>
2026-09-15 11:15:19 -0300

    express-mongo-2: Implementing page not found error handling middleware

--- 4-express-mongo-2/src/middlewares/manipuladorDeErros.js
+++ 4-express-mongo-2/src/middlewares/manipuladorDeErros.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import ErroBase from "../erros/erroBase.js";
 import RequisicaoIncoreta from "../erros/RequisicaoIncorreta.js";
 import ErroValidacao from "../erros/ErroValidacao.js";
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(erro, req, res, next) {
@@ -9,7 +10,10 @@ function manipuladorDeErros(erro, req, res, next) {
         new RequisicaoIncoreta().enviarResposta(res);
     } else if (erro instanceof mongoose.Error.ValidationError) {
         new ErroValidacao(erro).enviarResposta(res);
-    } else {
+    } else if (erro instanceof NaoEncontrado) {
+        erro.enviarResposta(res);
+    }
+    else {
         new ErroBase().enviarResposta(res);
     }
 }
