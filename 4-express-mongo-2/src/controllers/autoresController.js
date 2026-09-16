8bca8c0d3c6eb9d27575e36aa5669fe78d569546
joaovictormnzs <jvictormenezesl@gmail.com>
2026-09-16 13:01:38 -0300

    express-mongo-2: Updating handling of id not found case

--- 4-express-mongo-2/src/controllers/autoresController.js
+++ 4-express-mongo-2/src/controllers/autoresController.js
@@ -26,7 +26,6 @@ class AutorController {
             } else {
                 next(new NaoEncontrado("Id do Autor não localizado."));
             }
-
         } catch (erro) {
             next(erro);
         }
@@ -50,9 +49,14 @@ class AutorController {
         try {
             const id = req.params.id;
 
-            await autores.findByIdAndUpdate(id, { $set: req.body });
+            const autorResultado = await autores.findByIdAndUpdate(id, { $set: req.body });
 
-            res.status(200).send({ message: "Autor atualizado com sucesso" });
+            if (autorResultado != null) {
+                res.status(200).send({ message: "Autor atualizado com sucesso" });
+            } else {
+                next(new NaoEncontrado("Id do Autor não localizado."));
+            }
+
         } catch (erro) {
             next(erro);
         }
@@ -62,9 +66,13 @@ class AutorController {
         try {
             const id = req.params.id;
 
-            await autores.findByIdAndDelete(id);
+            const autorResultado = await autores.findByIdAndDelete(id);
 
-            res.status(200).send({ message: "Autor removido com sucesso" });
+            if (autorResultado != null ){
+                res.status(200).send({ message: "Autor removido com sucesso" });
+            } else {
+                next(new NaoEncontrado("Id do Autor não localizado."));
+            }
         } catch (erro) {
             next(erro);
         }
