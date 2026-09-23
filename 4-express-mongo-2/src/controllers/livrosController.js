5a5513c03bdba60b3315177ae27639f840f9a391
joaovictormnzs <jvictormenezesl@gmail.com>
2026-09-23 10:31:02 -0300

    express-mongo-2: implementing ordenation

--- 4-express-mongo-2/src/controllers/livrosController.js
+++ 4-express-mongo-2/src/controllers/livrosController.js
@@ -1,20 +1,32 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
     static listarLivros = async (req, res, next) => {
         try {
-            const { limite = 5, pagina = 1 }= req.query;
-            
-            
-            const livrosResultado = await livros.find()
-                .skip((pagina - 1) * limite)
-                .limit(limite)
-                .populate("autor")
-                .exec();
+            let { limite = 5, pagina = 1, ordenacao = "_id:-1"}= req.query;
 
-            res.status(200).json(livrosResultado);
+            let [campoOrdenacao, ordem] = ordenacao.split(":");
+            
+            limite = parseInt(limite);
+            pagina = parseInt(pagina);
+            ordem = parseInt(ordem);
+
+            if (limite > 0 && pagina > 0) {
+                const livrosResultado = await livros.find()
+                    .sort({ [campoOrdenacao]: ordem})
+                    .skip((pagina - 1) * limite)
+                    .limit(limite)
+                    .populate("autor")
+                    .exec();
+    
+                res.status(200).json(livrosResultado);
+            } else {
+                next(new RequisicaoIncorreta());
+            }
+
         } catch (erro) {
             next(erro);
         }
--- 4-express-mongo-2/src/middlewares/manipuladorDeErros.js
+++ 4-express-mongo-2/src/middlewares/manipuladorDeErros.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import ErroBase from "../erros/erroBase.js";
 import RequisicaoIncoreta from "../erros/RequisicaoIncorreta.js";
 import ErroValidacao from "../erros/ErroValidacao.js";
-import NaoEncontrado from "../erros/NaoEncontrado.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros(erro, req, res, next) {
@@ -10,10 +9,9 @@ function manipuladorDeErros(erro, req, res, next) {
         new RequisicaoIncoreta().enviarResposta(res);
     } else if (erro instanceof mongoose.Error.ValidationError) {
         new ErroValidacao(erro).enviarResposta(res);
-    } else if (erro instanceof NaoEncontrado) {
+    } else if (erro instanceof ErroBase) {
         erro.enviarResposta(res);
-    }
-    else {
+    } else {
         new ErroBase().enviarResposta(res);
     }
 }
